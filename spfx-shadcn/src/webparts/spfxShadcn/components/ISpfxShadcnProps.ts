31df2f6af238a920742b531570750480f68ef64c
Julien Bessière <syntax-error@live.com>
2024-08-16 17:40:40 +0200

    init spfx-shadcn

---
+++ spfx-shadcn/src/webparts/spfxShadcn/components/ISpfxShadcnProps.ts
@@ -0,0 +1,7 @@
+export interface ISpfxShadcnProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ spfx-shadcn/src/webparts/spfxShadcn/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpfxShadcnWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpfxShadcnWebPartStrings' {
+  const strings: ISpfxShadcnWebPartStrings;
+  export = strings;
+}
